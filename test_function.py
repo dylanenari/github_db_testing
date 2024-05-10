c43bb9f4876048531efc735076bd219ba8b65857
Dylan Enari <dylan.toral@amadeus.com>
2024-05-10 15:37:32 +0200

    Added testing functions to assert correct loading of the dataframe and data quality checks run.
    Change parameter names to match existing format. i.e. tableName -> table_name
    Add function to check if load is succesful
    Include necessary imports inside function
    Data quality function script to run unit tests

---
+++ test_function.py
@@ -0,0 +1,25 @@
+import pytest
+import pyspark
+from dq_function import *
+from pyspark.sql import SparkSession
+
+# Initialise Spark
+spark = SparkSession.builder \
+                    .appName('integrity-tests') \
+                    .getOrCreate()
+
+# parameters
+table_name = "raw_document"
+db_name = "ac_stg_green"
+module = "ti-tables-ticketing"
+date_column = "snapshot_date"
+airline_code = "AC"
+
+# Does the table exist?
+def test_tableExists():
+  assert tableExists(table_name, db_name) is True
+
+# Did the checks run?
+def test_quality_check():
+    results_df = quality_check(airline_code, module, table_name, date_column)
+    assert not results_df.isEmpty()
