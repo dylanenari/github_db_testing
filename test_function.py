ee41500492c84af2af46bb270cb84d5c59da5724
dylanenari <dylanenari>
2024-05-13 14:09:27 +0000

    explicitly declare fk_identifier to see if quality_check runs succesfully

--- test_function.py
+++ test_function.py
@@ -14,6 +14,7 @@ db_name = "ac_stg_green"
 module = "ti-tables-ticketing"
 date_column = "snapshot_date"
 airline_code = "AC"
+fk_identifier = None
 
 df = spark.sql(f"SELECT * FROM {db_name}.{table_name}") \
     .filter("snapshot_date_time >= '2023-01-01' and snapshot_date_time < '2023-01-08'") \
@@ -25,5 +26,5 @@ def test_tableExists():
 
 # did the checks run?
 def test_quality_check():
-  results_df = quality_check(df, airline_code, module, table_name, date_column)
+  results_df = quality_check(df, airline_code, module, table_name, date_column, fk_identifier)
   assert not results_df.isEmpty()
