fc4562cb7f6633143b2a733239ac8a8ab928ba77
Dylan Enari <dylan.toral@amadeus.com>
2024-05-14 12:44:50 +0200

    Added test for expected results_df columns

--- test_function.py
+++ test_function.py
@@ -17,13 +17,19 @@ airline_code = "AC"
 
 df = spark.sql(f"SELECT * FROM {db_name}.{table_name}") \
     .filter("snapshot_date_time >= '2023-01-01' and snapshot_date_time < '2023-01-08'") \
-    .withColumn("snapshot_date", col("snapshot_date_time").cast(DateType()))
+    .withColumn("snapshot_date", col("snapshot_date_time").cast(DateType()))\
+  
+results_df = quality_check(df, airline_code, module, table_name, date_column)
 
 # does the table exist?
 def test_tableExists():
-  assert tableExists(db_name, table_name) is True
+  assert tableExists(db_name, table_name) is True, "Table not found in database"
 
 # did the checks run?
 def test_quality_check():
-  results_df = quality_check(df, airline_code, module, table_name, date_column)
-  assert not results_df.isEmpty()
+  assert not results_df.isEmpty(), "Data quality results not generated succesfully"
+
+# does results_df have expected columns?
+def test_results_columns():
+  expected_columns = set([date_column, "airline_code", "module", "table", "kpi", "key", "value"])
+  assert results_df.columns == expected_columns, "Data quality results incomplete"
