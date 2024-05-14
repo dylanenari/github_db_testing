269c0aa7b7e6c39415123775cc0fd62d30f553d9
Dylan Enari <dylan.toral@amadeus.com>
2024-05-14 15:09:58 +0200

    Fixed indentation errors

--- dq_function.py
+++ dq_function.py
@@ -14,61 +14,62 @@ def tableExists(db_name, table_name):
 
 
 class QualityCheck:
-
-  def __init__(self, spark: SparkSession, df: pyspark.sql.DataFrame,
+    def __init__(self, spark: SparkSession, df: pyspark.sql.DataFrame,
                airline_code: str, module: str, table_name: str, date_column: str,
                fk_identifier: str = None):
-    self.spark = spark
-    self.df = df
-    self.airline_code = airline_code
-    self.module = module
-    self.table_name = table_name
-    self.date_column = date_column
-    self.fk_identifier = fk_identifier
+        self.spark = spark
+        self.df = df
+        self.airline_code = airline_code
+        self.module = module
+        self.table_name = table_name
+        self.date_column = date_column
+        self.fk_identifier = fk_identifier
 
-    # row count
-  def count_rows(self):
-    # ticketing
-    if "source" in self.df.columns:
+    # Row count
+    def count_rows(self):
+        # ticketing
+        if "source" in self.df.columns:
 
-        counts_df = self.df.groupBy(self.date_column, "source").agg(count("*").alias("value")) \
-            .withColumn("module", lit(self.module)) \
-            .withColumn("kpi", lit("row_count")) \
-            .withColumn("airline_code", lit(self.airline_code)) \
-            .withColumnRenamed("source", "key") \
-            .withColumn("table", lit(self.table_name)) \
-            .union(self.df.groupBy(self.date_column).agg(count("*").alias("value")) \
-                   .withColumn("module", lit(self.module)) \
-                   .withColumn("kpi", lit("row_count")) \
-                   .withColumn("airline_code", lit(self.airline_code)) \
-                   .withColumn("key", lit("general")) \
-                   .withColumn("table", lit(self.table_name)) \
-                   .select(self.date_column, "key", "value", "module", "kpi", "airline_code", "table"))
-    
-    # reservation
-    elif self.fk_identifier is not None and any(column for column in self.df.columns if self.fk_identifier in column):
+            counts_df = self.df.groupBy(self.date_column, "source").agg(count("*").alias("value")) \
+                .withColumn("module", lit(self.module)) \
+                .withColumn("kpi", lit("row_count")) \
+                .withColumn("airline_code", lit(self.airline_code)) \
+                .withColumnRenamed("source", "key") \
+                .withColumn("table", lit(self.table_name)) \
+                .union(self.df.groupBy(self.date_column).agg(count("*").alias("value")) \
+                    .withColumn("module", lit(self.module)) \
+                    .withColumn("kpi", lit("row_count")) \
+                    .withColumn("airline_code", lit(self.airline_code)) \
+                    .withColumn("key", lit("general")) \
+                    .withColumn("table", lit(self.table_name)) \
+                    .select(self.date_column, "key", "value", "module", "kpi", "airline_code", "table"))
         
-        fk_cols = [column for column in self.df.columns if self.fk_identifier in column]
+        # reservation
+        elif self.fk_identifier is not None and any(column for column in self.df.columns if self.fk_identifier in column):
+            
+            fk_cols = [column for column in self.df.columns if self.fk_identifier in column]
 
-        ## consistency
+            ## consistency
 
-        # counts by foreign keys and total
-        exprs = [count(c).alias(f"{c}") for c in fk_cols] + [count("*").alias("general")]
-        counts_df = self.df.groupBy(self.date_column).agg(*exprs)
+            # counts by foreign keys and total
+            exprs = [count(c).alias(f"{c}") for c in fk_cols] + [count("*").alias("general")]
+            counts_df = self.df.groupBy(self.date_column).agg(*exprs)
 
-        # transpose and add columns
-        counts_df = counts_df.melt(self.date_column, [column for column in counts_df.columns if column != self.date_column],
-                                   "key", "value") \
-            .withColumn("module", lit(self.module)) \
-            .withColumn("kpi", lit("row_count")) \
-            .withColumn("airline_code", lit(self.airline_code)) \
-            .withColumn("table", lit(self.table_name))
-    
+            # transpose and add columns
+            counts_df = counts_df.melt(self.date_column, [column for column in counts_df.columns if column != self.date_column],
+                                    "key", "value") \
+                .withColumn("module", lit(self.module)) \
+                .withColumn("kpi", lit("row_count")) \
+                .withColumn("airline_code", lit(self.airline_code)) \
+                .withColumn("table", lit(self.table_name))
+        
         return counts_df
 
-    # Duplicate count
-  def count_duplicates(self):
-        
+        # Duplicate count
+    
+    # Uniqueness
+    def count_duplicates(self):
+            
         #ticketing
         if "source" in self.df.columns:
             ## uniqueness
@@ -91,7 +92,7 @@ class QualityCheck:
         
         # reservation
         elif self.fk_identifier is not None and any(column for column in self.df.columns if self.fk_identifier in column):
-           
+            
             fk_cols = [column for column in self.df.columns if self.fk_identifier in column]            
 
             # filter duplicate rows and apply count expressions
@@ -119,73 +120,75 @@ class QualityCheck:
                 .select(self.date_column, "key", "value", "module", "kpi", "airline_code", "table")           
             
         return dupl_df
-
-    # Completeness
-  def compute_completeness(self):
-
-    # define date window
-    windowSpec = Window.partitionBy(self.date_column)
-    # loop through columns
-    for column in df.columns:
-        if column != self.date_column:
-            completeness_col = column + "_non_null_count"
-            # count column / count date = completeness over window
-            df = df.withColumn(completeness_col, (count(column).over(windowSpec) / count(self.date_column).over(windowSpec)))
-
-    # drop duplicates
-    compl_df = df.select(
-        [self.date_column] + [column for column in df.columns if "_non_null_count" in column]).dropDuplicates()
-    # delete _non_null_count from name to get original colu,mn
-    for column in compl_df.columns:
-        if "_non_null_count" in column:
-            compl_df = compl_df.withColumnRenamed(column, column.replace("_non_null_count", ""))
-
-    # melt, add columns
-    id_vars = self.date_column
-    values = [col for col in compl_df.columns if col != self.date_column]
-    vbleName = "key"
-    vlueName = "value"
-
-    if "source" in df.columns:
-        compl_df = compl_df.melt(id_vars, values, vbleName, vlueName) \
-            .withColumn("module", lit(self.module)) \
-            .withColumn("kpi", lit("completeness")) \
-            .withColumn("airline_code", lit(self.airline_code)) \
-            .withColumn("table", lit(self.table_name))
-    elif self.fk_identifier is not None and any(column for column in df.columns if self.fk_identifier in column):
-        compl_df = compl_df.melt(id_vars, values, vbleName, vlueName) \
-            .withColumn("module", lit(self.module)) \
-            .withColumn("kpi", lit("completeness")) \
-            .withColumn("airline_code", lit(self.airline_code)) \
-            .withColumn("table", lit(self.table_name))
-    else:
-        compl_df = compl_df.melt(id_vars, values, vbleName, vlueName) \
-            .withColumn("module", lit(self.module)) \
-            .withColumn("kpi", lit("completeness")) \
-            .withColumn("airline_code", lit(self.airline_code)) \
-            .withColumn("table", lit(self.table_name))
     
-    return compl_df
-  
-    # Timeliness
-def dates_check(self):
+    # Completeness
+    def compute_completeness(self):
 
-    # range of expected dates
-    min_date, max_date = self.df.select(min(self.date_column), max(self.date_column)).first()
-    date_range_df = spark.createDataFrame([(min_date, max_date)], ["min_date", "max_date"]) \
-        .select(explode(expr("sequence(to_date(min_date), to_date(max_date), interval 1 day)")).alias(self.date_column))
+        # define date window
+        windowSpec = Window.partitionBy(self.date_column)
+        # loop through columns
+        for column in df.columns:
+            if column != self.date_column:
+                completeness_col = column + "_non_null_count"
+                # count column / count date = completeness over window
+                df = df.withColumn(completeness_col, (count(column).over(windowSpec) / count(self.date_column).over(windowSpec)))
 
-    # dataframe of real dates
-    real_dates_df = self.df.select(self.date_column).distinct()
+        # drop duplicates
+        compl_df = df.select(
+            [self.date_column] + [column for column in df.columns if "_non_null_count" in column]).dropDuplicates()
+        # delete _non_null_count from name to get original colu,mn
+        for column in compl_df.columns:
+            if "_non_null_count" in column:
+                compl_df = compl_df.withColumnRenamed(column, column.replace("_non_null_count", ""))
 
-    # join to mach dates and union with other kpis
+        # melt, add columns
+        id_vars = self.date_column
+        values = [col for col in compl_df.columns if col != self.date_column]
+        vbleName = "key"
+        vlueName = "value"
 
-    dates_df = real_dates_df.join(date_range_df, self.date_column) \
-                .withColumn("key", lit("general")) \
-                .withColumn("value", when(col(self.date_column).isNull(), "Failure").otherwise("Success")) \
+        if "source" in df.columns:
+            compl_df = compl_df.melt(id_vars, values, vbleName, vlueName) \
                 .withColumn("module", lit(self.module)) \
-                .withColumn("kpi", lit("timeliness")) \
+                .withColumn("kpi", lit("completeness")) \
                 .withColumn("airline_code", lit(self.airline_code)) \
                 .withColumn("table", lit(self.table_name))
+        elif self.fk_identifier is not None and any(column for column in df.columns if self.fk_identifier in column):
+            compl_df = compl_df.melt(id_vars, values, vbleName, vlueName) \
+                .withColumn("module", lit(self.module)) \
+                .withColumn("kpi", lit("completeness")) \
+                .withColumn("airline_code", lit(self.airline_code)) \
+                .withColumn("table", lit(self.table_name))
+        else:
+            compl_df = compl_df.melt(id_vars, values, vbleName, vlueName) \
+                .withColumn("module", lit(self.module)) \
+                .withColumn("kpi", lit("completeness")) \
+                .withColumn("airline_code", lit(self.airline_code)) \
+                .withColumn("table", lit(self.table_name))
+        
+        return compl_df
+  
+        # Timeliness
     
-    return dates_df
+    # Timeliness
+    def dates_check(self):
+
+        # range of expected dates
+        min_date, max_date = self.df.select(min(self.date_column), max(self.date_column)).first()
+        date_range_df = spark.createDataFrame([(min_date, max_date)], ["min_date", "max_date"]) \
+            .select(explode(expr("sequence(to_date(min_date), to_date(max_date), interval 1 day)")).alias(self.date_column))
+
+        # dataframe of real dates
+        real_dates_df = self.df.select(self.date_column).distinct()
+
+        # join to mach dates and union with other kpis
+
+        dates_df = real_dates_df.join(date_range_df, self.date_column) \
+                    .withColumn("key", lit("general")) \
+                    .withColumn("value", when(col(self.date_column).isNull(), "Failure").otherwise("Success")) \
+                    .withColumn("module", lit(self.module)) \
+                    .withColumn("kpi", lit("timeliness")) \
+                    .withColumn("airline_code", lit(self.airline_code)) \
+                    .withColumn("table", lit(self.table_name))
+        
+        return dates_df
