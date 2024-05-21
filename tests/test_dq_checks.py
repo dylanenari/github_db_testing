b8989770f24c8d7406bc8c6cae960bc1c3330efc
dylanenari <dylanenari>
2024-05-21 12:45:38 +0000

    Modify logic in kpi generation tests
    Logical operators not implementing desired meaning in assertions

--- tests/test_dq_checks.py
+++ tests/test_dq_checks.py
@@ -28,7 +28,7 @@ def test_tableExists():
 # did the row count run?
 def test_rows_check():
   counts_df = QualityCheck(spark, df, airline_code, module, table_name, date_column).count_rows()
-  assert not counts_df.isEmpty() or counts_df == None, "Row count KPI not generated successfully"
+  assert counts_df is not None and not counts_df.isEmpty(), "Row count KPI not generated successfully"
 
 # do row count results have correct schema?
 def test_rows_cols():
@@ -39,7 +39,7 @@ def test_rows_cols():
 # did the duplicate count run?
 def test_duplicates_check():
   dupl_df = QualityCheck(spark, df, airline_code, module, table_name, date_column).count_duplicates()
-  assert not dupl_df.isEmpty() or dupl_df == None, "Duplicate count KPI not generated successfully"
+  assert cupl_df is not None and not dupl_df.isEmpty(), "Duplicate count KPI not generated successfully"
 
 # do duplicates count results have correct schema?
 def test_duplicates_cols():
@@ -50,7 +50,7 @@ def test_duplicates_cols():
 # did the completeness ratio run?
 def test_completeness_check():
   compl_df = QualityCheck(spark, df, airline_code, module, table_name, date_column).compute_completeness()
-  assert not compl_df.isEmpty() or compl_df == None, "Completeness ratio KPI not generated successfully"
+  assert compl_df is not None and not compl_df.isEmpty(), "Completeness ratio KPI not generated successfully"
 
 # do completeness ratio results have correct schema?
 def test_completeness_cols():
@@ -61,7 +61,7 @@ def test_completeness_cols():
 # did the timeliness run?
 def test_dates_check():
   dates_df = QualityCheck(spark, df, airline_code, module, table_name, date_column).dates_check()
-  assert not dates_df.isEmpty() or dates_df == None, "Date completeness KPI not generated successfully"
+  assert dates_df is not None and not dates_df.isEmpty(), "Date completeness KPI not generated successfully"
 
 # do dates results have correct schema?
 def test_dates_cols():
@@ -72,7 +72,7 @@ def test_dates_cols():
 # did all checks run and combine to produce results dataframe?
 def test_generate_results():
   results_df = QualityCheck(spark, df, airline_code, module, table_name, date_column).quality_check()
-  assert not results_df.isEmpty() or results_df == None, "DQ results not generated successfully"
+  assert results_df is not None and not results_df.isEmpty(), "DQ results not generated successfully"
 
 
 
